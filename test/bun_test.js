62d48751a4116c3c627a00809d26e5f25c7cc44f
naomik <nakyoto@gmail.com>
2013-03-13 15:55:41 -0500

    Initial commit

---
+++ test/bun_test.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+
+describe("bun", function() {
+
+  it("should be the best", function() {
+    var bun = "is the best";
+    assert.strictEqual(bun, "is the best");
+  });
+
+});
